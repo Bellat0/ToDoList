ad43698341bfd2abc66ccc33d7cff04e6e20fdd0
MaximTvilinev <maximtvilinev@gmail.com>
2025-12-23 15:24:13 +0500

    Удалил main, Разбил проект по MVC

---
+++ ToDoList/TasksList/Controller/TasksViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Maxim Tvilinev on 23.12.2025.
+//
+
+import UIKit
+import SnapKit
+
+class TasksViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Dependencies
+    
+    // MARK: - Init
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupConstraints()
+//        setupTableView()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
+
